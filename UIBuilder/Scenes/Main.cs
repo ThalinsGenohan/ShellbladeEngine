526cc5c3b02ea9a8b517d10b7e897a14c3f9dfd6
Sushi <weingaben@gmail.com>
2021-03-19 04:27:00 +0100

    Added empty main window functionality

---
+++ UIBuilder/Scenes/Main.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using SFML.System;
+using Shellblade;
+
+namespace UIBuilder.Scenes
+{
+    internal class Main : Scene
+    {
+        public Main(Game window) : base(window)
+        {
+
+        }
+
+        public override void Loop(Time dt)
+        {
+            
+        }
+    }
+}
--- UIBuilder/UIBuilder.cs
+++ UIBuilder/UIBuilder.cs
@@ -1,10 +1,26 @@
-﻿namespace UIBuilder
+﻿using UIBuilder.Scenes;
+using Shellblade;
+using Shellblade.Graphics;
+using Shellblade.Graphics.UI;
+
+namespace UIBuilder
 {
     internal class UIBuilder
     {
-        static void Main(string[] args)
-        {
+        private const uint _resX = 320;
+        private const uint _resY = 240;
+        private const uint _scale = 4;
+        private const string _fontDir = "assets/fonts/";
 
+        private static uint _windowX => _resX * _scale; //maybe restructure so Window_ only gets recalculated
+        private static uint _windowY => _resY * _scale; //when Res_ changes? sfml probably has something for this
+
+        private static void Main(string[] args)
+        {
+            var window = new Game(_windowX, _windowY, _resX, _resY, "UI Builder");
+            window.LoadScene(new Main(window));
+
+            window.Run();
         }
     }
 }
