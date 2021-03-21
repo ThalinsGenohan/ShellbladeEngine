3dd7cd2e9ddbf5282e75e99efa9643b8153bd92c
Sushi <weingaben@gmail.com>
2021-03-22 00:41:48 +0100

    Changed box to non abstract class, moved stuff from FilledBox over

--- ShellbladeEngine/Graphics/UI/Box.cs
+++ ShellbladeEngine/Graphics/UI/Box.cs
@@ -2,11 +2,47 @@
 using System.Collections.Generic;
 using System.Text;
 using SFML.Graphics;
+using SFML.System;
 
 namespace Shellblade.Graphics.UI
 {
-	public abstract class Box : UIElement
+	public class Box : UIElement
 	{
+		internal RectangleShape rectangle;
 
+		public override Vector2i GlobalPosition
+		{
+			get => base.GlobalPosition;
+			set
+			{
+				base.GlobalPosition = value;
+				rectangle.Position = (Vector2f)value;
+			}
+		}
+
+		public override Vector2i Size
+		{
+			get => base.Size;
+			set
+			{
+				base.Size = value;
+				rectangle.Size = (Vector2f)value;
+			}
+		}
+
+		public Box()
+        {
+			rectangle = new RectangleShape(new Vector2f(0f, 0f))
+			{
+				Position = (Vector2f)GlobalPosition,
+				Size = (Vector2f)Size
+			};
+        }
+
+		public override void Draw(RenderTarget target, RenderStates states)
+		{
+			target.Draw(rectangle, states);
+			base.Draw(target, states);
+		}
 	}
 }
--- ShellbladeEngine/Graphics/UI/FilledBox.cs
+++ ShellbladeEngine/Graphics/UI/FilledBox.cs
@@ -5,48 +5,15 @@ namespace Shellblade.Graphics.UI
 {
 	public class FilledBox : Box
 	{
-		private readonly RectangleShape _rectangle;
-
-		public override Vector2i GlobalPosition
-		{
-			get => base.GlobalPosition;
-			set
-			{
-				base.GlobalPosition       = value;
-				_rectangle.Position = (Vector2f)value;
-			}
-		}
-
-		public override Vector2i Size
-		{
-			get => base.Size;
-			set
-			{
-				base.Size       = value;
-				_rectangle.Size = (Vector2f)value;
-			}
-		}
-
 		public Color Color
 		{
-			get => _rectangle.FillColor;
-			set => _rectangle.FillColor = value;
+			get => rectangle.FillColor;
+			set => rectangle.FillColor = value;
 		}
 
-		public FilledBox()
+		public FilledBox() : base()
 		{
-			_rectangle = new RectangleShape(new Vector2f(0f, 0f))
-			{
-				FillColor = Color.White,
-				Position  = (Vector2f)GlobalPosition,
-				Size      = (Vector2f)Size,
-			};
-		}
-
-		public override void Draw(RenderTarget target, RenderStates states)
-		{
-			target.Draw(_rectangle, states);
-			base.Draw(target, states);
+			rectangle.FillColor = Color.White;
 		}
 	}
 }
