26b467b10d50a8fb741f85c776aa8e03292b7113
Sushi <weingaben@gmail.com>
2021-03-22 02:26:11 +0100

    Added right-click-menu base

--- UIBuilder/Scenes/Main.cs
+++ UIBuilder/Scenes/Main.cs
@@ -12,12 +12,30 @@ namespace UIBuilder.Scenes
     internal class Main : Scene
     {
         //private Button testButton { get; }
-        
+
+        private Window _window;
+
+        private bool _menu;
+        private bool _rMouseDown;
+        private Vector2i _menuPos;
+        private int _scale;
+
+        private OutlinedBox menu = new OutlinedBox
+        {
+            FillColor = new Color(0x5C1AE1a5),
+            Color = new Color(0x5C1AE1ff),
+            OutlineThickness = 2f,
+            Size = new Vector2i(60, 100)
+        };
+
 
         public Main(Game window) : base(window)
         {
             //Set default cursor
             window.SetCursor(@"assets/cursor_arrow.png", 0, 0, window);
+
+            _window = window.GetWindow();
+            _scale = (int)(window.WindowSize.X / window.Resolution.X);
 
             /*
             testButton = new Button(new Vector2i(16, 16),
@@ -35,14 +53,41 @@ namespace UIBuilder.Scenes
 
             Input.UI = new UIContainer
             {
-                Elements = new Dictionary<string, UIElement>
-                {}
+                Elements = new Dictionary<string, UIElement>{}
             };
         }
 
         public override void Loop(Time dt)
         {
-            
+            //Menu handling
+            if (Mouse.IsButtonPressed(Mouse.Button.Right))
+            {
+                _rMouseDown = true;
+
+                if (Mouse.GetPosition(_window)/_scale != _menuPos)
+                {
+                    _menu = false;
+                    Input.UI.Elements.Remove("menu");
+                }
+            }
+            else if (Mouse.IsButtonPressed(Mouse.Button.Left))
+            {
+                _menu = false;
+                _rMouseDown = false;
+                Input.UI.Elements.Remove("menu");
+            }
+
+            if (_rMouseDown && !Mouse.IsButtonPressed(Mouse.Button.Right) && !_menu)
+            {
+                _menu = true;
+                _rMouseDown = false;
+                _menuPos = Mouse.GetPosition(_window)/_scale;
+                if (!Input.UI.Elements.ContainsKey("menu"))
+                {
+                    menu.GlobalPosition = _menuPos;
+                    Input.UI.Elements.Add("menu", menu);
+                }
+            }
         }
     }
 }
