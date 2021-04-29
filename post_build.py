7a04e0c90182eefce64274dd559585cccfe048e1
Thalins Genohan <32170993+ThalinsGenohan@users.noreply.github.com>
2021-04-29 05:18:12 -0400

    Update post_build.py

--- post_build.py
+++ post_build.py
@@ -6,6 +6,6 @@ project_dir = sys.argv[1]
 target_dir = sys.argv[2]
 
 if(platform.system() == "Windows"):
-  os.system("rmdir /q /s {target_dir}assets;xcopy /e /i /y {project_dir}assets {target_dir}assets".format(target_dir = target_dir, project_dir = project_dir))
+  os.system("rmdir {target_dir}assets /s /q & xcopy {project_dir}assets {target_dir}assets /i /s /e /y".format(target_dir = target_dir, project_dir = project_dir))
 else:
   os.system("rm -rf {target_dir}assets;cp -R {project_dir}assets {target_dir}".format(target_dir = target_dir, project_dir = project_dir))
