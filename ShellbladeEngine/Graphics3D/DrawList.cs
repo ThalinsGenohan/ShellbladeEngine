9aaf08acabb7d800a67cf87aafd73b009429f8cf
Thalins Genohan <32170993+ThalinsGenohan@users.noreply.github.com>
2022-04-23 05:28:41 -0400

    Create DrawList.cs

---
+++ ShellbladeEngine/Graphics3D/DrawList.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Shellblade.Graphics3D.GPU;
+
+namespace Shellblade.Graphics3D;
+
+internal class DrawList
+{
+	public List<Model> Models { get; }
+
+	private BufferObject      _modelsBufferObject;
+	private VertexArrayObject _vao;
+
+	internal void Stitch()
+	{
+		
+	}
+}
