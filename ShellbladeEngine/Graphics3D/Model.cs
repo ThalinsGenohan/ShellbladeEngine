731c3909ad24d59b76a8a6bffa2f8cd9538c47e2
Thalins Genohan <32170993+ThalinsGenohan@users.noreply.github.com>
2022-04-23 05:04:07 -0400

    Create Model.cs

---
+++ ShellbladeEngine/Graphics3D/Model.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shellblade.Graphics3D
+{
+	internal class Model
+	{
+		public Transform    Transform { get; set; }
+		public List<Vertex> Vertices  { get; set; }
+	}
+}
